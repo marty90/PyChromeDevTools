efae30039d38af7b6a1aca64eabe301eda3911d0
Martino Trevisan <martino.trevisan@polito.it>
2017-06-08 14:01:43 +0200

    Improved Return Value of calls

--- PyChromeDevTools.py
+++ PyChromeDevTools.py
@@ -6,6 +6,8 @@ import time
 
 TIMEOUT=1
 
+message_id=0
+
 class GenericElement(object):
     def __init__(self,name,parent):
         self.name=name
@@ -13,40 +15,42 @@ class GenericElement(object):
     def __getattr__(self, attr):
         func_name=self.name+"."+attr
         def GenericFunction(**args):
+            global message_id
             self.parent.pop_messages()
-            call_obj={"id": 0, "method": func_name, "params": args}
-            self.parent.soc.send(json.dumps(call_obj))
-            return json.loads(self.parent.soc.recv())
+            message_id+=1
+            call_obj={"id": message_id, "method": func_name, "params": args}
+            self.parent.ws.send(json.dumps(call_obj))
+            result,_=self.parent.wait_result(message_id)
+            return result
         return GenericFunction
 
 class ChromeInterface(object):
     def __init__(self, host='localhost', port=9222,tab=0):
         self.host = host
         self.port = port
-        self.soc = None
-        self.tablist = None
-        self.find_tabs()
+        self.ws = None
+        self.tabs = None
+        self.get_tabs()
         self.connect(tab=tab)
-    def find_tabs(self):
+    def get_tabs(self):
         response = requests.get("http://%s:%s/json" % (self.host, self.port))
-        self.tablist = json.loads(response.text)
+        self.tabs = json.loads(response.text)
     def connect(self, tab=0, update_tabs=True):
-        wsurl = self.tablist[tab]['webSocketDebuggerUrl']
-        self.soc = websocket.create_connection(wsurl)
-        self.soc.settimeout(TIMEOUT)
+        wsurl = self.tabs[tab]['webSocketDebuggerUrl']
+        self.ws = websocket.create_connection(wsurl)
+        self.ws.settimeout(TIMEOUT)
     def close(self):
-        if self.soc:
-            self.soc.close()
-            self.soc = None
+        if self.ws:
+            self.ws.close()
     # Blocking
     def wait_message(self, timeout=TIMEOUT):
-        self.soc.settimeout(timeout)
+        self.ws.settimeout(timeout)
         try:
-            message = self.soc.recv()
+            message = self.ws.recv()
         except:
-            self.soc.settimeout(TIMEOUT)
+            self.ws.settimeout(TIMEOUT)
             return None
-        self.soc.settimeout(TIMEOUT)
+        self.ws.settimeout(TIMEOUT)
         return json.loads(message)
     # Blocking
     def wait_event(self, event,timeout=TIMEOUT):
@@ -58,7 +62,7 @@ class ChromeInterface(object):
             if now-start_time>timeout:
                 break
             try:
-                message = self.soc.recv()
+                message = self.ws.recv()
                 parsed_message=json.loads(message)
                 messages.append(parsed_message)
                 if "method" in parsed_message and parsed_message["method"]==event:
@@ -67,23 +71,41 @@ class ChromeInterface(object):
             except:
                 break
         return (matching_message, messages)
+    # Blocking
+    def wait_result(self, result_id,timeout=TIMEOUT):
+        start_time=time.time()
+        messages=[]
+        matching_result=None
+        while True:
+            now=time.time()
+            if now-start_time>timeout:
+                break
+            try:
+                message = self.ws.recv()
+                parsed_message=json.loads(message)
+                messages.append(parsed_message)
+                if "result" in parsed_message and parsed_message["id"]==result_id:
+                    matching_result=parsed_message
+                    break
+            except:
+                break
+        return (matching_result, messages)
 
     # Non Blocking
     def pop_messages(self):
         messages=[]
-        self.soc.settimeout(0)
+        self.ws.settimeout(0)
         while True:
             try:
-                message = self.soc.recv()
+                message = self.ws.recv()
                 messages.append(json.loads(message))
             except:
                 break
-        self.soc.settimeout(TIMEOUT)
+        self.ws.settimeout(TIMEOUT)
         return messages
 
     def __getattr__(self, attr):
         genericelement=GenericElement(attr,self)
         self.__setattr__(attr,genericelement)
         return genericelement
-
 
