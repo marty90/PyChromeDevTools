5d413c2e03906c7137eef154f1ae481d2ae2c952
David Higgins <sligodave@gmail.com>
2017-06-30 01:11:47 -0600

    Added setup.py and made the module a package so it can be installed with pip.

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="PyChromeDevTools",
+    description="PyChromeDevTools : Interact with Google Chrome using the Chrome DevTools Protocol.",
+    license="Apache License 2.0",
+    version="0.1",
+    author="Martino Trevisan",
+    author_email="martino.trevisan@polito.it",
+    maintainer="Martino Trevisan",
+    maintainer_email="martino.trevisan@polito.it",
+    url="https://github.com/marty90/PyChromeDevTools",
+    packages=['PyChromeDevTools'],
+    install_requires=['requests', 'websocket']
+)
