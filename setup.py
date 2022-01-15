1b3f9bc7e420cf70e1333a9fb0db0f002f46e9d9
Martino Trevisan <martino.trevisan@polito.it>
2022-01-15 10:25:29 +0100

    new release

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 try:
     from setuptools import setup
 except ImportError:
@@ -8,13 +7,13 @@ setup(
     name="PyChromeDevTools",
     description="PyChromeDevTools : Interact with Google Chrome using the Chrome DevTools Protocol.",
     license="Apache License 2.0",
-    version="0.3",
+    version="0.4",
     author="Martino Trevisan",
     author_email="martino.trevisan@polito.it",
     maintainer="Martino Trevisan",
     maintainer_email="martino.trevisan@polito.it",
     url="https://github.com/marty90/PyChromeDevTools",
-    download_url = 'https://github.com/marty90/PyChromeDevTools/tarball/1.0.1',
+    download_url = 'https://github.com/marty90/PyChromeDevTools/tarball/1.0.2',
     packages=['PyChromeDevTools'],
     install_requires=['requests', 'websocket-client']
 )
